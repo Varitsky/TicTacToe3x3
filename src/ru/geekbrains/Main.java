412c693cf75dbe83755355885576b15e711edfd4
Varitsky <Varitsky@gmail.com>
2020-05-17 14:54:43 +0300

    Выбирается знак игрока. Если знак 0 - то компьютер ходит первым. Счётчик ходов ограничивает последний ход.

--- src/ru/geekbrains/Main.java
+++ src/ru/geekbrains/Main.java
@@ -9,16 +9,48 @@ public class Main {
     public static int sizeOfField = 3;
 
     public static void main(String[] args) {
-        loadOurEmptyFieldWithSize();
-        printOurField();
-        char userSign = '\u0058';       // 0
-        char computerSign = '\u004F';   // X
-        int countOfMoves = 0;
+        letsPlay();
+    }
 
+        public static void letsPlay(){
+        char[] signs = yourSign();
+        playTicTacToe(signs[0], signs[1]);
+    }
+
+    public static char[] yourSign() {
+        char[] signs = new char[2];
+        Random rand = new Random();
+        int x = new Random().nextInt(2);
+        if (x == 0) {
+            signs[0] = '\u0058';
+            signs[1] = '\u004F';
+        } else {
+            signs[0] = '\u004F';
+            signs[1] = '\u0058';
+        }
+        return signs;
+    }
+
+
+    public static void playTicTacToe(char userSign, char computerSign) {
+        loadOurEmptyFieldWithSize();
+        int countOfMoves = 0;
+        if (userSign=='\u004F'){
+            System.out.println("Ваш знак - "+userSign+", так что Компьютер ходит первым, вот он и сходил - ");
+            computerMove(computerSign);
+            countOfMoves = countOfMoves + 1;
+        } else
+            System.out.println("Ваш знак - "+userSign+", так что ваш ход первый.");
+        printOurField();
         do {
             yourMove(userSign);
-            countOfMoves=countOfMoves+1;
-            if(countOfMoves==9){
+            countOfMoves = countOfMoves + 1;
+            if (countOfMoves == 9) {
+                if (theWinnerIsFound(gameField)) {
+                    printOurField();
+                    System.out.println("Победа за тобой");
+                    break;
+                }
                 System.out.println("Место кончилось, победитель не выявлен");
                 printOurField();
                 break;
@@ -30,8 +62,13 @@ public class Main {
             }
             computerMove(computerSign);
             printOurField();
-            countOfMoves=countOfMoves+1;
-            if(countOfMoves==9){
+            countOfMoves = countOfMoves + 1;
+            if (countOfMoves == 9) {
+                if (theWinnerIsFound(gameField)) {
+                    printOurField();
+                    System.out.println("Победа за тобой");
+                    break;
+                }
                 System.out.println("Место кончилось, победитель не выявлен");
                 printOurField();
                 break;
@@ -75,13 +112,13 @@ public class Main {
         do {
             x = yourMoveX();
             y = yourMoveY();
-            if (!checkTheCell(x,y)){
-                System.out.println("\n"+"\n"+"\n"+"\n"+"\n"+"Клетка уже занята!");
+            if (!checkTheCell(x, y)) {
+                System.out.println("\n" + "\n" + "\n" + "\n" + "\n" + "Клетка уже занята!");
                 System.out.println("Вот что у нас сейчас:");
                 printOurField();
             }
         } while (!checkTheCell(x, y));
-        System.out.println("\n"+"\n"+"\n"+"\n"+"\n"+"\n");
+        System.out.println("\n" + "\n" + "\n" + "\n" + "\n" + "\n");
         gameField[y][x] = userSign;
     }
 
@@ -130,31 +167,28 @@ public class Main {
                     return true;
             }
         }
-        int tempotempo = 0;
+        int tempotempo = 0; // проверка первой диагонали
         for (int i = 0; i < sizeOfField; i++) {
-            for (int j = 0; j < sizeOfField; j++) {
-                if (gameField[i][j]==gameField[0][0] && gameField[0][0]!='\u00B7'){
-                    tempotempo = tempotempo + 1;
-                    if(tempotempo==3){
-                        return true;
-                    }
-            }
-        }
-
-        }
-
-        int tempotempo2 = 0;
-        for (int i = 0; i < sizeOfField; i++) {
-            for (int j = sizeOfField-1; j >=0; j--) {
-                if (gameField[i][j]==gameField[sizeOfField-1][0] && gameField[sizeOfField-1][0]!='\u00B7'){
-                    tempotempo2 = tempotempo2 + 1;
-                    if(tempotempo2==3){
-                        return true;
-                    }
+            if (gameField[i][i] == gameField[0][0] && gameField[0][0] != '\u00B7') {
+                tempotempo = tempotempo + 1;
+                if (tempotempo == 3) {
+                    return true;
                 }
             }
 
         }
+
+        int tempotempo2 = 0;  // проверка второй диагонали
+        for (int i = sizeOfField - 1; i >= 0; i--) {
+            if (gameField[i][sizeOfField - 1 - i] == gameField[1][1] && gameField[1][1] != '\u00B7') {
+                tempotempo2 = tempotempo2 + 1;
+                if (tempotempo2 == 3) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 }
+
+
